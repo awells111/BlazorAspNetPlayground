a9921d63bcb88ef6a2edf955fede5447965ae09c
Alex Wells <awells111@gmail.com>
2020-12-02 22:45:43 -0500

    Added Todo

---
+++ BlazorAspNetPlayground/Client/Models/Tutorials/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAspNetPlayground.Client.Models.Tutorials
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
